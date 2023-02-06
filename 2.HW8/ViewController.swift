ed159179f631b275ff3d806cabbf9c058d1f0559
MIA <mamatalievaindira1@gmail.com>
2023-02-06 23:43:07 +0600

    Initial Commit

---
+++ 2.HW8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.HW8
+//
+//  Created by Indira on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
